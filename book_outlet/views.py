de1b9dac94a3066cd71aa176e5a0fafe87f493c7
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-19 01:16:44 +0530

    added aggregate functions in models and views

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,14 +1,19 @@
 from django.http.response import Http404
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Avg
 
 from .models import Book
 # Create your views here.
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-title")
+    num_of_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
     return render(request, 'book_outlet/index.html', {
-        "books": books
+        "books": books,
+        "avg_rating": avg_rating,
+        "no_of_books": num_of_books
     })
 
 
