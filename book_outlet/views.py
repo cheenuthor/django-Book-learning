a3873437c8e2e6e4dfe49ab5ab0fb7fbaf9af35f
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-18 23:45:33 +0530

    added book-detail page with Url and added bootstrap

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Book
 # Create your views here.
@@ -9,3 +10,18 @@ def index(request):
     return render(request, 'book_outlet/index.html', {
         "books": books
     })
+
+
+def book_details(request, id):
+
+    # * here we can use id or pk(primary key)
+    book = get_object_or_404(Book, pk=id)
+    return render(request, 'book_outlet/book-detail.html',
+                  {
+                      "title": book.title,
+                      "author": book.author,
+                      "rating": book.rating,
+                      "is_bestseller": book. is_best_selling
+                  }
+                  )
+ 
