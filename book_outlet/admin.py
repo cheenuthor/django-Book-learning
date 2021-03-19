544f7828b3e81b32224bcb172af16725566b2032
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-19 14:20:18 +0530

    config model for admin area

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,4 +1,4 @@
-from book_outlet.models import Book
+from book_outlet.models import Author, Book
 from django.contrib import admin
 
 # Register your models here.
@@ -7,8 +7,15 @@ from django.contrib import admin
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = {"slug": ("title",)}
-    list_filter =("author","rating")
-    list_display = ("title","author","rating")
+    list_filter = ("author", "rating")
+    list_display = ("title", "author", "rating")
+
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ("first_name", "last_name")
 
 
 admin.site.register(Book, BookAdmin)
+admin.site .register(Author,
+                     #  AuthorAdmin
+                     )
